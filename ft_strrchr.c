8de2ca0d2155d91b95128645c2c1a269d09d00a4
yildirimmmm <my570124124@gmail.com>
2025-05-31 14:53:20 +0300

    ft_strrchr: Implement ft_strrchr to locate last occurrence of a character in a string

---
+++ ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 17:44:28 by muhyildi          #+#    #+#             */
+/*   Updated: 2025/05/31 11:50:27 by muhyildi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+	int	last_index;
+
+	i = 0;
+	last_index = -1;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			last_index = i;
+		i++;     
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	if (last_index != -1)
+		return ((char *)&s[last_index]);
+	return (NULL);
+}
