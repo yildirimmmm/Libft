a4913fd297f08e647483bcaa349b102cfb46de3d
yildirimmmm <my570124124@gmail.com>
2025-06-01 12:30:33 +0300

    ft_strlcat: Implement ft_strlcat to concatenate strings with size limitation

---
+++ ft_strlcat.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/31 16:25:02 by muhyildi          #+#    #+#             */
+/*   Updated: 2025/06/01 12:29:28 by muhyildi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (dstsize == 0)
+		return (src_len);
+	while (dst_len + i < dstsize - 1 && src[i] != '\0')
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	if (dst_len < dstsize)
+		dst[dst_len + i] = '\0';
+	if (dst_len < dstsize)
+		return (dst_len + src_len);
+	else
+		return (dstsize + src_len);
+}
