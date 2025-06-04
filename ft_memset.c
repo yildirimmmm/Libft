07003bd8679b95689fd9bf74d217d0fa313c5fa1
yildirimmmm <my570124124@gmail.com>
2025-06-04 03:56:26 +0300

    ft_memset: Implement ft_memset to fill memory with a constant byte

---
+++ ft_memset.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 14:41:27 by muhyildi          #+#    #+#             */
+/*   Updated: 2025/06/04 03:55:59 by muhyildi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char *dst;
+
+	dst = (unsigned char *)s;
+	while (n--)
+		*dst++ = (unsigned char)c;
+	return (dst);
+}
+ 
