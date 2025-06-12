faedaaae550d4b5834e5992edfb21496c48128ad
yildirimmmm <my570124124@gmail.com>
2025-06-12 19:20:45 +0300

    ft_memchr: Implement ft_memchr to search memory for a specific byte

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 09:56:02 by muhyildi          #+#    #+#             */
+/*   Updated: 2025/06/12 10:08:10 by muhyildi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*src;
+
+	src = (unsigned char *)s;
+	while (n--)
+	{
+		if (*src == (unsigned char)c)
+			return ((void *)src);
+		src++;
+	}
+	return (NULL);		
+}
