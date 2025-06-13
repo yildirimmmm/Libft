1cb4ef937999a56b29e8f76f662fefecb0665b3b
yildirimmmm <my570124124@gmail.com>
2025-06-13 23:35:09 +0300

    ft_memcmp: Implement ft_memcmp to compare memory regions byte-by-byte

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/13 23:22:56 by muhyildi          #+#    #+#             */
+/*   Updated: 2025/06/13 23:34:11 by muhyildi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
+	while (n--)
+	{
+		if (*ptr1 != *ptr2)
+			return ((int)*ptr1 - (int)*ptr2);
+		ptr1++;
+		ptr2++;
+	}
+	return (0);
+}
