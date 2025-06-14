663365d5240c386111250609a90c9109dc4192db
yildirimmmm <my570124124@gmail.com>
2025-06-14 04:37:45 +0300

    static: Remove duplicate static functions and use shared prototypes from

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,23 +6,11 @@
 /*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 03:50:17 by muhyildi          #+#    #+#             */
-/*   Updated: 2025/06/14 04:02:31 by muhyildi         ###   ########.fr       */
+/*   Updated: 2025/06/14 04:37:10 by muhyildi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 
 char	*ft_strdup(const char *s)
 {
