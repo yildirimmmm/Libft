ca560fe49fd8004d6dab6664ac5b248a13cc3013
yildirimmmm <my570124124@gmail.com>
2025-06-14 04:36:21 +0300

    static: Remove duplicate static functions and use shared prototypes from

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,20 +6,11 @@
 /*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 23:38:41 by muhyildi          #+#    #+#             */
-/*   Updated: 2025/06/14 03:45:57 by muhyildi         ###   ########.fr       */
+/*   Updated: 2025/06/14 04:34:17 by muhyildi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void	ft_static_bzero(void *s, size_t n)
-{
-	unsigned char	*adr;
-
-	adr = (unsigned char *)s;
-	while (n--)
-		*adr++ = 0;
-}
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
@@ -30,7 +21,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	dst = malloc(allocate_size);
 	if (dst != NULL)
 	{
-		ft_static_bzero(dst, allocate_size);
+		ft_bzero(dst, allocate_size);
 		return (dst);
 	}
 	return (NULL);
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,23 +6,11 @@
 /*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 14:48:25 by muhyildi          #+#    #+#             */
-/*   Updated: 2025/06/03 12:46:45 by muhyildi         ###   ########.fr       */
+/*   Updated: 2025/06/14 04:34:38 by muhyildi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
