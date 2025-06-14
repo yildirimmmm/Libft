4610698ae484c03791863922366bfbf3e340c991
yildirimmmm <my570124124@gmail.com>
2025-06-14 06:39:22 +0300

    ft_strtrim: Implement ft_strtrim to remove specified leading and trailing characters

---
+++ ft_strtrim.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhyildi <muhyildi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 05:50:43 by muhyildi          #+#    #+#             */
+/*   Updated: 2025/06/14 06:38:41 by muhyildi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	is_in_set(char const c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i] != '\0')
+		if (c == set[i])
+			return (1);
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	start;
+	size_t	end;
+	char	*dst;
+
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	i = 0;
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && is_in_set(s1[start], set))
+		start++;
+	while (end > start && is_in_set(s1[end - 1], set))
+		end--;
+	dst = malloc((end - start) + 1);
+	if (dst == NULL)
+		return (NULL);
+	while (start < end)
+		dst[i++] = s1[start++];
+	dst[i] = '\0';
+	return (dst);
+}
